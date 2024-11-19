652ce70267befa39b29bd468b26df0c2a5503961
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-11-19 19:41:22 +0100

    implementing saving json file

--- app/main.py
+++ app/main.py
@@ -1,31 +1,38 @@
-import datetime
 import argparse
 import json
+
+from datetime import datetime
 
 
 class TaskTracker:
     def __init__(self):
-        self.parser = argparse.ArgumentParser()
+        self.tasks_file = 'tasks.json'
+        self.parser = argparse.ArgumentParser(prog='TaskTracker')
         self._add_arguments()
         self.tasks = dict()
 
     def _add_arguments(self):
-        self.parser.add_argument('-a', '--add')
-        self.parser.add_argument('-u', '--update')
+        self.parser.add_argument('-a', '--add', nargs='+', type=str, help='Add task')
+        self.parser.add_argument('-u', '--update', nargs='+', type=int, help='Update task')
         self.parser.add_argument('-d', '--delete')
+
+    def save(self):
+        with open(self.tasks_file, 'w') as f:
+            json.dump(self.tasks, f)
 
     def run(self):
         args = self.parser.parse_args()
-
-        if args.add:
-            self.tasks[args.add] = {'id': '',
-                                    'description': '',
-                                    'createdAt': datetime.datetime.now(),
-                                    'updatedAt': datetime.datetime.now()
-                                    }
+        for arg in args.add:
+            self.tasks[len(self.tasks) + 1] = {'id': len(self.tasks) + 1,
+                                               'title': arg,
+                                               'description': '',
+                                               'status': '',
+                                               'createdAt': datetime.now().isoformat(),
+                                               'updatedAt': datetime.now().isoformat(),
+                                               }
+        self.save()
 
 
 if __name__ == '__main__':
     tasks = TaskTracker()
     tasks.run()
-
