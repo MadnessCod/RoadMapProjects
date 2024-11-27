c19ab27e7b4d312bd4f850d7a36fa3c23b2af285
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-11-27 23:08:28 +0100

    Added listing and best score for each mode, fixed some bugs

--- app/main.py
+++ app/main.py
@@ -13,10 +13,16 @@ class RandomNumberGenerator:
         self.add_argument()
 
     def add_argument(self):
-        self.parser.add_argument('-m', '--mode', choices=['easy', 'medium', 'hard'])
+        self.parser.add_argument(
+            '-m',
+            '--mode',
+            choices=['easy', 'medium', 'hard'],
+            help='Mode of the game, choices are easy, medium, hard')
         self.parser.add_argument('-u', '--username', help='Users name')
+        self.parser.add_argument('-l', '--list', action='store_true', help='List all user\'s game')
+        self.parser.add_argument('-b', '--best', help='Get the lowest attempt of a users for different modes')
 
-    def save(self, attempt):
+    def save(self, attempt=None):
         args = self.parser.parse_args()
         user = {
             'User': args.username,
@@ -24,9 +30,9 @@ class RandomNumberGenerator:
             'Time': datetime.datetime.now().strftime('%H:%M:%S'),
             'Mode': args.mode,
             'Number': self.number,
-            'Guesses': 'failed' if attempt is None else attempt,
+            'Guesses': -1 if attempt is None else attempt,
         }
-        self.load()
+        self.users = self.load()
         self.users.append(user)
         with open(self.file, 'w', newline='') as csvfile:
             writer = csv.DictWriter(
@@ -45,7 +51,7 @@ class RandomNumberGenerator:
             print('File not found')
         return self.users
 
-    def game(self, username, mode, number):
+    def game(self, number):
         for attempt in range(number):
             try:
                 guess = int(input('Enter your guess: '))
@@ -59,30 +65,50 @@ class RandomNumberGenerator:
                     elif guess < self.number:
                         print('Too low')
                     elif guess == self.number:
-                        print(f'Correct the number was {self.number}')
+                        print(f'Correct, the number was {self.number}')
                         self.save(attempt)
-                        break
+                        return
                 else:
                     print(f'Hey enter a number between 1 and 100')
                     print('Don\'t worry I won\'t deduct a point for that')
                     attempt -= 1
         print(f'The game has finished, you could\'t guess the number, the number was {self.number}')
-        self.save(attempt=None)
+        self.save()
+
+    def best_score(self, entries):
+        for type_ in ['easy', 'medium', 'hard']:
+            filtered = list(filter(lambda entry: entry['Mode'] == type_, entries))
+            try:
+                print(min([int(i['Guesses']) for i in filtered if int(i['Guesses']) > 0]))
+            except ValueError:
+                print(f'There is no attempt for {type_} mode')
 
     def run(self):
         args = self.parser.parse_args()
         if args.username:
             if args.mode == 'easy':
                 print('You have chosen Easy Mode you have 10 chances ')
-                self.game(args.username, args.mode, 10)
+                self.game(10)
             elif args.mode == 'medium':
                 print('You have chosen Medium Mode you have 5 chances')
-                self.game(args.username, args.mode, 5)
+                self.game(5)
             elif args.mode == 'hard':
                 print('You have chosen Hard Mode you have 3 chances')
-                self.game(args.username, args.mode, 3)
+                self.game(3)
             else:
                 print('you haven\'t chosen any mode')
+        if args.list:
+            self.users = self.load()
+            for user in self.users:
+                print(f'User: {user["User"]}, Date: {user["Date"]}, Time: {user["Time"]}, '
+                      f'Mode: {user["Mode"]}, Number: {user["Number"]}, Guesses: {user["Guesses"]}')
+        if args.best:
+            self.users = self.load()
+            filtered = list(filter(lambda person: person['User'] == args.best, self.users))
+            if filtered:
+                self.best_score(filtered)
+            else:
+                print(f'There is no such user : {args.best}')
 
 
 if __name__ == '__main__':
