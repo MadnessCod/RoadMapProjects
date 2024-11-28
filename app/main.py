4443feed31c93314d8f4f15e91b6713df5b069d5
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-11-28 19:17:04 +0100

    added documentation

--- app/main.py
+++ app/main.py
@@ -8,23 +8,35 @@ from typing import Dict
 
 
 class TaskTracker:
+    """TaskTracker class
+    Attributes:
+        tasks : dict
+            Dictionary of tasks, keyed by task id
+        tasks_file : json file with tasks
+        parser : argparse parser
+    """
     def __init__(self) -> None:
         self.tasks_file = 'tasks.json'
-        self.parser = argparse.ArgumentParser(prog='TaskTracker')
+        self.parser = argparse.ArgumentParser(
+            prog='TaskTracker',
+            description='Keep track of your tasks, program saves tasks in a json file',
+        )
         self.tasks = dict()
         self._add_arguments()
 
     def _add_arguments(self) -> None:
-        self.parser.add_argument('-a', '--add', type=str, help='Add task')
-        self.parser.add_argument('-u', '--update', type=str, help='Update task by it id ')
+        """Adds arguments to the parser"""
+        self.parser.add_argument('-a', '--add', type=str, help='Add a task')
+        self.parser.add_argument('-u', '--update', type=str, help='Update task by it id')
         self.parser.add_argument(
             '-s',
             '--status',
             type=str,
             choices=['in progress', 'done'],
-            help='Set status of a task, use it with --update'
+            help='Set status of a task, use it with --update, choices are "in progress", "done"',
         )
         self.parser.add_argument(
+            'de',
             '--description',
             type=str,
             help='add description for a task, use it with --update'
@@ -34,7 +46,7 @@ class TaskTracker:
             '-l',
             '--list',
             action='store_true',
-            help='List all tasks'
+            help='List all tasks from json file'
         )
         self.parser.add_argument(
             '-l_d',
@@ -56,16 +68,19 @@ class TaskTracker:
         )
 
     def save(self) -> None:
+        """Saves tasks to json file"""
         with open(self.tasks_file, 'w') as f:
             json.dump(self.tasks, f)
 
     def load(self) -> Dict:
+        """Loads tasks from json file"""
         if os.path.exists(self.tasks_file):
             with open(self.tasks_file, 'r') as f:
                 return json.load(f)
         return dict()
 
     def add_task(self, task) -> None:
+        """Adds a tasks from provided commandline arguments"""
         self.tasks = self.load()
         task_id = len(self.tasks) + 1
         self.tasks[task_id] = {'id': task_id,
@@ -78,6 +93,7 @@ class TaskTracker:
         self.save()
 
     def list_tasks(self) -> None:
+        """reads tasks from json file and prints them on CLI"""
         self.tasks = self.load()
         for task in self.tasks:
             print(
@@ -90,14 +106,18 @@ class TaskTracker:
             )
 
     def delete_task(self, task_id) -> None:
+        """Deletes a task by its id
+            Raises KeyError if task_id is not found
+        """
         self.tasks = self.load()
         try:
             del self.tasks[task_id]
             self.save()
         except KeyError:
-            print('Task not found')
+            print(f'Task with {task_id} not found')
 
     def update_status(self, task_id, status) -> None:
+        """Update a task status by its id"""
         self.tasks = self.load()
         for task in self.tasks:
             if task == task_id:
@@ -105,9 +125,10 @@ class TaskTracker:
                 self.tasks[task]['updatedAt'] = datetime.now().isoformat()
                 self.save()
             else:
-                print('Task not found')
+                print(f'Task with {task_id} not found')
 
     def update_description(self, task_id, description) -> None:
+        """"Updates a task description by its id"""
         self.tasks = self.load()
         for task in self.tasks:
             if task == task_id:
@@ -115,13 +136,13 @@ class TaskTracker:
                 self.tasks[task]['updatedAt'] = datetime.now().isoformat()
                 self.save()
             else:
-                print('Task not found')
+                print(f'Task with {task_id} not found')
 
     def run(self) -> None:
+        """"Runs the tasks list"""
         args = self.parser.parse_args()
         if args.add:
             self.add_task(args.add)
-
         if args.list:
             self.list_tasks()
         if args.list_done:
