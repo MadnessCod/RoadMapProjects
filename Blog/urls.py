0ef6cbb038be7c3b093b55ca66046118e1a3e45c
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-12-02 19:45:29 +0100

    planning the basics

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 
+from Blog import views
 
-urlpatterns = []
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('<int:article_di>/', views.article, name='article'),
+    path('admin/', views.admin, name='admin'),
+    path('<int:article_id>/', views.add, name='add'),
+    path('new/', views.new, name='new'),
+]
--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    pass
+
+
+def article(request, article_id):
+    pass
+
+
+def admin(request):
+    pass
+
+
+def add(request):
+    pass
+
+
+def new(request, article_id):
+    pass
