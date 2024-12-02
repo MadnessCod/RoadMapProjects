9516475e62ff3b26b9d2c6547e00ded9c582517a
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-12-02 20:01:02 +0100

    created Author and BaseModel models

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+
+    class Meta:
+        abstract = True
+        ordering = ('pk',)
+
+
+class Article(BaseModel):
+    title = models.CharField(max_length=200, verbose_name='Title')
+    content = models.TextField(verbose_name='Content')
+    author = models.CharField(max_length=200, verbose_name='Author')
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return self.title
