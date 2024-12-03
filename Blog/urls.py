79fd9dc66b5e4484cb1276f0586e2badb9df0239
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-12-03 12:48:01 +0100

    implementing login, logout, signup function

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 from Blog import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('articles/', views.home, name='articles'),
     path('<int:article_di>/', views.article, name='article'),
-    path('admin/', views.admin, name='admin'),
+    path('dashboard/', views.admin, name='admin'),
     path('<int:article_id>/', views.add, name='add'),
     path('new/', views.new, name='new'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
+
+from .models import Article
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
 
 def home(request):
-    pass
+    return render(request, 'articles.html')
 
 
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login/')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+@login_required(login_url='login')
 def article(request, article_id):
-    pass
+    try:
+        articles = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'articles.html', {'articles': articles})
 
 
 def admin(request):
