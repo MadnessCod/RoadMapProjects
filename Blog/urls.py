500dfee5b2b1565f4b762147f9b0c04155de4109
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-12-07 20:45:49 +0100

    added some functionallity

--- Blog/urls.py
+++ Blog/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('<int:article_id>/', views.article, name='article'),
     path('dashboard/', views.admin, name='admin'),
     path('add_article/', views.add, name='add_article'),
+    path('<int:article_id>/edit/', views.edit_article, name='edit_article'),
+    path('<int:article_id>/delete/', views.delete_article, name='delete_article'),
     path('new/', views.new, name='new'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -25,7 +25,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login/')
+            return redirect('login')
     else:
         form = CustomUserCreationForm()
     return render(request, 'signup.html', {'form': form})
@@ -34,10 +34,45 @@ def signup(request):
 @login_required(login_url='login')
 def article(request, article_id):
     try:
-        article_content = Article.objects.get(pk=article_id)
+        article = Article.objects.get(pk=article_id)
+        is_author = article.author == request.user
+        print(is_author)
     except Article.DoesNotExist:
         raise Http404
-    return render(request, 'article.html', {'article': article_content})
+    return render(request, 'article.html', {'article': article, 'is_author': is_author})
+
+
+@login_required()
+def edit_article(request, article_id):
+    article = Article.objects.get(pk=article_id)
+
+    if article.author != request.user:
+        return HttpResponse(f'You can\'t edit this article, because the author is {article.author} and the user is {request.user.id} and {article.author == request.user}')
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('article', article_id=article.id)
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'edit_article.html', {"form": form, 'article': article})
+
+
+@login_required()
+def delete_article(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404
+
+    if article.author != request.user:
+        return HttpResponse("You can\'t delete this article.")
+
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles')  # Redirect to the blogs page after deletion
+    return render(request, 'confirm_delete.html', {'article': article})
 
 
 def admin(request):
