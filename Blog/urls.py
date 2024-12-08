5a8e7ae008165138554f6d45091c7eb6c340bff4
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-12-08 15:32:09 +0100

    Fixed edit and delete function

--- Blog/urls.py
+++ Blog/urls.py
@@ -6,13 +6,11 @@ from Blog import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('articles/', views.home, name='articles'),
     path('<int:article_id>/', views.article, name='article'),
-    path('dashboard/', views.admin, name='admin'),
     path('add_article/', views.add, name='add_article'),
     path('<int:article_id>/edit/', views.edit_article, name='edit_article'),
     path('<int:article_id>/delete/', views.delete_article, name='delete_article'),
-    path('new/', views.new, name='new'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -9,34 +9,32 @@ from .forms import CustomUserCreationForm, ArticleForm
 
 
 # Create your views here.
-
-def home(request):
-    try:
-        articles = Article.objects.all()
-    except Article.DoesNotExist:
-        raise Http404('Webpage does not exist')
-
-    return render(request, 'articles.html', {'articles': articles})
-
-
 def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login')
+            return redirect('articles')
     else:
         form = CustomUserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
 
 @login_required(login_url='login')
+def home(request):
+    try:
+        articles = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404('Webpage does not exist')
+    return render(request, 'articles.html', {'articles': articles})
+
+
+@login_required(login_url='login')
 def article(request, article_id):
     try:
         article = Article.objects.get(pk=article_id)
-        is_author = article.author == request.user
-        print(is_author)
+        is_author = article.author == request.user.username
     except Article.DoesNotExist:
         raise Http404
     return render(request, 'article.html', {'article': article, 'is_author': is_author})
@@ -45,10 +43,8 @@ def article(request, article_id):
 @login_required()
 def edit_article(request, article_id):
     article = Article.objects.get(pk=article_id)
-
-    if article.author != request.user:
-        return HttpResponse(f'You can\'t edit this article, because the author is {article.author} and the user is {request.user.id} and {article.author == request.user}')
-
+    if article.author != request.user.username:
+        return HttpResponse(f'You can\'t edit this article')
     if request.method == 'POST':
         form = ArticleForm(request.POST, instance=article)
         if form.is_valid():
@@ -65,18 +61,13 @@ def delete_article(request, article_id):
         article = Article.objects.get(pk=article_id)
     except Article.DoesNotExist:
         raise Http404
-
-    if article.author != request.user:
+    if article.author != request.user.username:
         return HttpResponse("You can\'t delete this article.")
 
     if request.method == 'POST':
         article.delete()
         return redirect('articles')  # Redirect to the blogs page after deletion
     return render(request, 'confirm_delete.html', {'article': article})
-
-
-def admin(request):
-    pass
 
 
 @login_required()
@@ -91,7 +82,3 @@ def add(request):
     else:
         form = ArticleForm()
     return render(request, 'add_article.html', {'form': form})
-
-
-def new(request, article_id):
-    pass
