5e8f3a6d4332110cfe0847e834dd1a4e878cb70e
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-12-12 12:09:13 +0100

    modified validate_date, improved query build for request

--- app/main.py
+++ app/main.py
@@ -19,9 +19,9 @@ class WeatherAPI:
                                  help='location attribute for weather data shema: {London,Uk}')
         self.parser.add_argument('-la', '--latitude', type=validate_latitude,
                                  help='Provide location with longitude and latitude')
-        self.parser.add_argument('-d1', '--date1', type=validate_date,
+        self.parser.add_argument('-s', '--start', type=validate_date,
                                  help='Initial date to get weather data shema: YYYY-MM-DD')
-        self.parser.add_argument('-d2', '--date2', type=validate_date,
+        self.parser.add_argument('-e', '--end', type=validate_date,
                                  help='Final date to get weather data shema: YYYY-MM-DD')
 
     def save(self, data):
@@ -34,15 +34,27 @@ class WeatherAPI:
         except requests.exceptions.RequestException as e:
             print(f'Error : {e}')
         else:
-            if response.status_code != requests.codes.ok:
-                print('There is no information with the data you provided')
+            status = response.status_code // 100
+            if status == 4:
+                print('Client error')
+            elif status == 5:
+                print('Server error')
             else:
                 self.save(response.json())
 
     def run(self):
         self.args = self.parser.parse_args()
         if self.args.location or self.args.latitude:
-            self.base_url = f'{self.base_url}{self.args.location}?key={self.key}'
+            self.base_url = f'{self.base_url}{self.args.location or self.args.latitude}'
+            if self.args.start and self.args.end:
+                if self.args.start < self.args.end:
+                    print(f'Start date: {self.args.start} should be smaller than End date: {self.args.end}')
+                    return
+            if self.args.start:
+                self.base_url = f'{self.base_url}/{self.args.start}'
+            if self.args.end:
+                self.base_url = f'{self.base_url}/{self.args.end}'
+            self.base_url = f'{self.base_url}?key={self.key}'
             self.request()
         else:
             print('Either location or latitude must be provided')
@@ -50,26 +62,40 @@ class WeatherAPI:
 
 def validate_location(value):
     if ',' not in value:
-        raise argparse.ArgumentTypeError('The entry you provided is not a comma-separated')
+        raise argparse.ArgumentTypeError(f'The entry you provided is not in right format: {value}, Shema : London,UK')
+    elif len(value.split(',')) != 2:
+        raise argparse.ArgumentTypeError('You can provide one location at a time')
     return value
 
 
 def validate_latitude(value):
     if ',' not in value:
-        raise argparse.ArgumentTypeError('The entry you provided is not a comma-separated list')
+        raise argparse.ArgumentTypeError('The entry you provided is not in right format: {value}, Shema : '
+                                         'numbers:numbers')
     elif len(value.split(',')) != 2:
         raise argparse.ArgumentTypeError('You can provide one location at a time')
     elif not all([i.isnumeric() for i in value.split(',')]):
         raise argparse.ArgumentTypeError(
-            f'The value you provided should be both numbers, values: {[i for i in value.split(',')]}')
+            f'The value you provided should be both numbers, values: {value}')
     return value
 
 
 def validate_date(value):
+    formats = ['%Y-%m-%d', '%Y-%m-%dT%H:%M:%S']
+    for format_ in formats:
+        try:
+            return datetime.datetime.strptime(value, format_).date()
+        except ValueError:
+            continue
+
     try:
-        return datetime.datetime.strptime(value, '%Y-%m-%d').date()
-    except ValueError:
-        raise argparse.ArgumentTypeError('The entry you provided is not a valid date')
+        timestamp = int(value)
+        return datetime.datetime.fromtimestamp(timestamp)
+    except (ValueError, OverflowError):
+        raise argparse.ArgumentTypeError(
+            f'Invalid date format: {value}\n'
+            f'Valid formats are {" or ".join(formats)} or Unix timestamp'
+        )
 
 
 if __name__ == '__main__':
