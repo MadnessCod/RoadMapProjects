d7352934ba4dee69db71dcc9a4b9f8ce88246ce8
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-12-12 11:11:53 +0100

    added validate function for parser arguments

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import json
 
 import requests
 
+import datetime
 from local_settings import API_KEY
 
 
@@ -14,10 +15,14 @@ class WeatherAPI:
         self.add_argument()
 
     def add_argument(self):
-        self.parser.add_argument('-l', '--location', required=True, type=validate_comma,
-                                 help='location attribute for weather data\nshema: {\nLondon,Uk\n}')
-        self.parser.add_argument('-d1', '--date1', type=str, help='Initial date to get weather data')
-        self.parser.add_argument('-d2', '--date2', type=str, help='Final date to get weather data')
+        self.parser.add_argument('-l', '--location', type=validate_location,
+                                 help='location attribute for weather data shema: {London,Uk}')
+        self.parser.add_argument('-la', '--latitude', type=validate_latitude,
+                                 help='Provide location with longitude and latitude')
+        self.parser.add_argument('-d1', '--date1', type=validate_date,
+                                 help='Initial date to get weather data shema: YYYY-MM-DD')
+        self.parser.add_argument('-d2', '--date2', type=validate_date,
+                                 help='Final date to get weather data shema: YYYY-MM-DD')
 
     def save(self, data):
         with open(f'{self.args.location}.json', 'w') as f:
@@ -36,14 +41,35 @@ class WeatherAPI:
 
     def run(self):
         self.args = self.parser.parse_args()
-        self.base_url = f'{self.base_url}{self.args.location}?key={self.key}'
-        self.request()
+        if self.args.location or self.args.latitude:
+            self.base_url = f'{self.base_url}{self.args.location}?key={self.key}'
+            self.request()
+        else:
+            print('Either location or latitude must be provided')
 
 
-def validate_comma(value):
+def validate_location(value):
     if ',' not in value:
-        raise argparse.ArgumentTypeError('The location you entered is incorrect')
+        raise argparse.ArgumentTypeError('The entry you provided is not a comma-separated')
     return value
+
+
+def validate_latitude(value):
+    if ',' not in value:
+        raise argparse.ArgumentTypeError('The entry you provided is not a comma-separated list')
+    elif len(value.split(',')) != 2:
+        raise argparse.ArgumentTypeError('You can provide one location at a time')
+    elif not all([i.isnumeric() for i in value.split(',')]):
+        raise argparse.ArgumentTypeError(
+            f'The value you provided should be both numbers, values: {[i for i in value.split(',')]}')
+    return value
+
+
+def validate_date(value):
+    try:
+        return datetime.datetime.strptime(value, '%Y-%m-%d').date()
+    except ValueError:
+        raise argparse.ArgumentTypeError('The entry you provided is not a valid date')
 
 
 if __name__ == '__main__':
