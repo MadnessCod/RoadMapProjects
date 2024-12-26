9b8e9972f4b0cec7aacdafb1872afdbc8392ab79
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-12-26 11:29:33 +0100

    created models

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from .models import Tag, Post
+
 
 # Register your models here.
+
+@register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    search_fields = ('name',)
+
+
+@register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'tags')
+    search_fields = ('title', 'tags')
+    list_filter = ('tags',)
--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-created_at',)
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Name')
+
+    class Meta:
+        ordering = ('pk',)
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    def __str__(self):
+        return self.name
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=250, null=False, blank=False, verbose_name='Title')
+    content = models.TextField(null=False, blank=False, verbose_name='Content')
+    tags = models.ForeignKey(
+        'Tag',
+        verbose_name='Tags',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
