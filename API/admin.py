9f6fdce148a8c3067e2d101cb8ce39c04c1aeb0e
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-12-30 10:00:58 +0100

    defining models

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from .models import TodoList
+
 
 # Register your models here.
+
+@register(TodoList)
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+    list_display_links = ('id', 'title')
--- API/models.py
+++ API/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BaseMode(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return NotImplemented('str method not implemented')
+
+
+class TodoList(BaseMode):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Todo List'
+        verbose_name_plural = 'Todo Lists'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return self.title
+
