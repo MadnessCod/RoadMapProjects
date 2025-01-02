f03c43f196f0c2b124f1d8ba695224abc89deb25
Sadra Hosseini <sadrahosseini98@gmail.com>
2025-01-02 11:30:39 +0100

    Integrated Add and update todo

--- API/urls.py
+++ API/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import register, login, add_todo
+from .views import register, login, add_todo, update_todo
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
-    path('add/', add_todo, name='add'),
+    path('todos/', add_todo, name='add'),
+    path('todos/<int:todo_id>', update_todo, name='update'),
 ]
--- API/views.py
+++ API/views.py
@@ -78,15 +78,63 @@ def add_todo(request):
         token = request.headers.get('Authorization')
         if not token:
             return JsonResponse({'message': 'Unauthorized'}, status=401)
+        try:
+            user = User.objects.get(token=token)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'invalid token'}, status=401)
 
         if not data.get('title') or not data.get('description'):
             return JsonResponse({'message': 'Missing required field'}, status=401)
 
-        todo = TodoList.objects.create(title=data.get('title'), description=data.get('description'))
+        todo = TodoList.objects.create(
+            title=data.get('title'),
+            description=data.get('description'),
+            author=user
+        )
 
         return JsonResponse({
             'id': todo.id,
             'title': todo.title,
-            'description': todo.description, },
+            'description': todo.description,
+        },
+            status=201)
+    return JsonResponse({'error': 'Invalid HTTP method'}, status=405)
+
+
+@csrf_exempt
+def update_todo(request, todo_id):
+    if request.method == 'PUT':
+        if not request.headers.get('Authorization'):
+            return JsonResponse({'message': 'Unauthorized'}, status=401)
+
+        try:
+            user = User.objects.get(token=request.headers.get('Authorization'))
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'invalid token'}, status=401)
+
+        try:
+            data = json.loads(request.body)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'error': 'massage not valid'}, status=400)
+
+        if not data.get('title') or not data.get('description'):
+            return JsonResponse({'message': 'Missing required field'}, status=401)
+
+        try:
+            todo = TodoList.objects.get(id=todo_id)
+        except TodoList.DoesNotExist:
+            return JsonResponse({'error': 'invalid id'}, status=401)
+
+        if data.get('title'):
+            todo.title = data.get('title')
+        if data.get('description'):
+            todo.description = data.get('description')
+
+        todo.save()
+        return JsonResponse({
+            'id': todo.id,
+            'title': todo.title,
+            'description': todo.description,
+        },
             status=201)
     return JsonResponse({'error': 'Invalid HTTP method'}, status=405)
