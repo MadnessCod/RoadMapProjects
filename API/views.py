118f1351e403807a6e23656dcbb396e1a39f8f90
Sadra Hosseini <sadrahosseini98@gmail.com>
2025-01-15 16:06:42 +0100

    testing with JWT

--- API/views.py
+++ API/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json
+
+from django.contrib.auth.models import User
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        pass
+
+
+class HomeView(APIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            'message': f'Hello {user.username}',
+            'user': {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+            }
+        }, status=status.HTTP_200_OK)
