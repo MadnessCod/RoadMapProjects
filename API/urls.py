8d011ca367b3b3ccf6c8c3d612df7c2ae579a967
Sadra Hosseini <sadrahosseini98@gmail.com>
2025-01-19 08:59:21 +0100

    added PUT, PATCH and DELETE methods

--- API/urls.py
+++ API/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .views import RegisterView, ExpenseView
+from .views import RegisterView, ExpenseView, ExpenseUpdate, ExpenseDelete
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('expenses/', ExpenseView.as_view(), name='expenses'),
+    path('expenses/<int:pk>/update/', ExpenseUpdate.as_view(), name='expense-update'),
+    path('expenses/<int:pk>/delete/', ExpenseDelete.as_view(), name='expense_delete'),
 ]
--- API/views.py
+++ API/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView, RetrieveUpdateAPIView
 
 from .models import Expense
 from .serializers import UserRegisterSerializer, ExpenseSerializer
@@ -37,3 +37,24 @@ class ExpenseView(ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class ExpenseUpdate(RetrieveUpdateAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
+
+
+class ExpenseDelete(DestroyAPIView):
+    queryset = Expense.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
